b6efaa1b74737e15ae69d9242653065a0d3acbb5
Freddy Kellison-Linn <fred.kl@me.com>
2014-08-10 01:55:22 -0700

    handle errors for read()

--- readmsg.c
+++ readmsg.c
@@ -6,17 +6,21 @@
 
 ssize_t readmsg(int fd, void *buf, size_t len, char *dlim) {
     char c;
+    ssize_t n;
     size_t i = 0;
     char *str = (char *)buf;
-    while (read(fd, &c, 1) && i < len) {
+    while ((n = read(fd, &c, 1)) > 0 && i < len) {
         str[i++] = c;
 	int di = 0;
-	while (dlim[di] && dlim[di++] == c && read(fd, &c, 1)) { 
+	while (dlim[di] && dlim[di++] == c && (n = read(fd, &c, 1)) > 0) { 
 	    str[i++] = c;
 	}
-	if (di == strlen(delim)) {
+	if (di == strlen(delim) || n < 0) {
 	    break;
 	}
+    }
+    if (n < 0) {
+	return n;
     }
     return i;
 }
