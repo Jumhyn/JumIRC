e100932d8d40c5c21b569ed187f1ad52219bbcc1
Freddy Kellison-Linn <fred.kl@me.com>
2014-08-10 01:52:12 -0700

    remove unused var

--- readmsg.c
+++ readmsg.c
@@ -4,14 +4,14 @@
 #include <stdlib.h>
 #include "readmsg.h"
 
-ssize_t readmsg(int fd, void *buf, size_t len, char *delim) {
+ssize_t readmsg(int fd, void *buf, size_t len, char *dlim) {
     char c;
     size_t i = 0;
     char *str = (char *)buf;
     while (read(fd, &c, 1) && i < len) {
         str[i++] = c;
-	int dmatch = 0, di = 0;
-	while (delim[di] && delim[di++] == c && read(fd, &c, 1)) { 
+	int di = 0;
+	while (dlim[di] && dlim[di++] == c && read(fd, &c, 1)) { 
 	    str[i++] = c;
 	}
 	if (di == strlen(delim)) {
